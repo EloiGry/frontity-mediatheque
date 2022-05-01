f1fb8b71b302e3e907aa43e1af17f85310a132b8
EloiGry <eloi.grychta@gmail.com>
2022-05-01 17:37:53 +0200

    modif

--- packages/bibli-theme/src/components/Footer.js
+++ packages/bibli-theme/src/components/Footer.js
@@ -19,8 +19,8 @@ const Footer = () => {
             <Plan>
                 <p> <b> Accès </b></p>
                 <br/>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2659.671076578411!2d3.9913249149971333!3d48.19368865523126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ee906bb707d23d%3A0x42fce8397bf06b38!2sImp.%20de%20la%20M%C3%A9diath%C3%A8que%2C%2010320%20Bouilly%2C%20France!5e0!3m2!1sfr!2sar!4v1651274655778!5m2!1sfr!2sar" 
-                width="200" height="150" style={{borderRadius: "20px"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2659.671076578411!2d3.9913249149971333!3d48.19368865523126!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ee906bb707d23d%3A0x42fce8397bf06b38!2sImp.%20de%20la%20M%C3%A9diath%C3%A8que%2C%2010320%20Bouilly%2C%20France!5e0!3m2!1sfr!2sar!4v1651274655778!5m2!1sfr!2sar" 
+                width="200" height="150" style={{borderRadius: "20px"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe> */}
                 <br/>
                 <p> Adresse : Impasse de la Médiathèque </p>
             </Plan>
